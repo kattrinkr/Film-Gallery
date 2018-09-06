b7284ffd1a19fdb6f17d19f35d96bfe44c7609c1
Kathryn <36640350+kattrinkr@users.noreply.github.com>
2018-09-06 10:07:33 +0300

    Update controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,11 @@
 import Films from '../models/filmModel'
 
+const wrapper = promise => (
+    promise
+      .then(film => ({ film, error: null }))
+      .catch(error => ({ error, film: null }))
+)
+
 async function getFilms(req, res) {
     const page = req.params.page ? req.params.page : 1;
     const limit = 4;
@@ -14,19 +20,14 @@ async function getFilms(req, res) {
     if (req.params.film) {
         search.title = new RegExp( `^${req.params.film}((([A-Za-z]+)?(\\s)?)+)?$`, 'i');
     }
-    try {
-        const filmItems = await Films.find(search).sort(sort).limit(limit).skip(page*limit-limit);
-        res.json(filmItems);
-    } catch (err) {
-        res.status(500).send(err);
+    const { error, film } = await wrapper(Films.find(search).sort(sort).limit(limit).skip(page*limit-limit));
+    if (!error) {
+        res.json(film);
+            return;
+    } else {
+         res.status(500).send(error)
     }
 }
-
-const wrapper = promise => (
-    promise
-      .then(film => ({ film, error: null }))
-      .catch(error => ({ error, film: null }))
-)
 
 async function getOneFilm(req, res) {
     if (req.params.id) {  
@@ -35,32 +36,38 @@ async function getOneFilm(req, res) {
             res.json(film);
             return;
         } else {
-            console.error(error)
+            res.status(500).send(error)
         } 
     }
 }
 
 async function updateFilmRating(req, res) {
-    if (req.params.id) {     
-        try {
-            let film = await Films.findById(req.params.id);
-            const newRating = Math.round((+film.rating + +req.body.rating) / 2);
-            film = await Films.findOneAndUpdate({_id: req.params.id}, {rating: newRating});
-            res.json(film);
-        } catch (err) {
-            res.status(500).send(err);
-        }
+    if (req.params.id) {  
+        let { error, filmById } = await wrapper(Films.findById(req.params.id));
+        const newRating = Math.round((+filmById.rating + +req.body.rating) / 2);
+        if (!error) {
+            let { error, film } = await wrapper(Films.findOneAndUpdate({_id: req.params.id}, {rating: newRating}));
+            if (!error) {
+                res.json(film);
+                return;
+            } else {
+                res.status(500).send(error)
+            }
+        } else {
+            res.status(500).send(error)
+        }   
     }
 }
 
 async function addComment(req, res) {
-    if (req.params.id) {     
-        try {
-            const film = await Films.findOneAndUpdate({_id: req.params.id}, {$push: {comments: req.body.comment}});
+    if (req.params.id) {   
+        const { error, film } = await wrapper(Films.findOneAndUpdate({_id: req.params.id}, {$push: {comments: req.body.comment}}));
+        if (!error) {
             res.json(film);
-        } catch (err) {
-            res.status(500).send(err);
-        }
+            return;
+        } else {
+            res.status(500).send(error)
+        } 
     }
 }
 
